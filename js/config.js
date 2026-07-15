8b312f0c3fc0d8122589309bc3d72b8d9dcc9628
Damsl18 <davidshamba18@gmail.com>
2026-07-15 21:01:41 +0100

    Fix missing closing quote in PUBLIC_APP_URL

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@
 const CONFIG = {
   API_BASE_URL: 'https://ecokin-api.onrender.com/api',
   UPLOADS_BASE_URL: 'https://ecokin-api.onrender.com/',
-  PUBLIC_APP_URL: 'https://ecokin-public.netlify.app,
+  PUBLIC_APP_URL: 'https://ecokin-public.netlify.app',
 };
